14e56dbb061a69309b381c244da0b515db7d3e81
irongineer <daichi.hotchi@gmail.com>
2020-12-26 10:55:51 +0900

    Fix client app id

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-1_wRap8Ydeq', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1fnatsn6e7ti3273r5fl3ueqgl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '20thal6gqs0d74oseh52ubol7d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
